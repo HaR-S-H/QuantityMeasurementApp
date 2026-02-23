b2094a884b817124f9c23ba1f82f9434ec439221
HaR-S-H <h8551283@gmail.com>
2026-02-23 10:43:23 +0530

    [Harsh Gupta].Add done uc4

--- QuantityMeasurementApp/Models/LengthUnit.cs
+++ QuantityMeasurementApp/Models/LengthUnit.cs
@@ -7,17 +7,30 @@ namespace QuantityMeasurementApp.Models
     public enum LengthUnit
     {
         Feet,
-        Inch
+        Inch,
+        Yard,
+          Centimeter
     }
 
     public static class LengthUnitExtensions
     { // Extension method to convert LengthUnit to a factor for conversion to feet
         public static double ToFeetFactor(this LengthUnit unit)
-        { // Return the conversion factor to feet based on the specified LengthUnit
+        {
+            // Return the conversion factor to feet based on the specified LengthUnit
             return unit switch
             {
                 LengthUnit.Feet => 1.0,
+
+                // 12 inches = 1 foot
                 LengthUnit.Inch => 1.0 / 12.0,
+
+                // 1 yard = 3 feet
+                LengthUnit.Yard => 3.0,
+
+                // 1 cm = 0.393701 inch
+                // convert inch → feet
+                LengthUnit.Centimeter => 0.393701 / 12.0,
+                // Throw an exception for unsupported units
                 _ => throw new ArgumentException("Unsupported unit")
             };
         }
--- QuantityMeasurementApp/Program.cs
+++ QuantityMeasurementApp/Program.cs
@@ -5,9 +5,14 @@ class Program
 {
     static void Main()
     {
-           var q1 = new QuantityLength(1.0, LengthUnit.Feet);
-        var q2 = new QuantityLength(12.0, LengthUnit.Inch);
+      var yard = new QuantityLength(1, LengthUnit.Yard);
+        var feet = new QuantityLength(3, LengthUnit.Feet);
+        var inches = new QuantityLength(36, LengthUnit.Inch);
+        var cm = new QuantityLength(1, LengthUnit.Centimeter);
+        var inch = new QuantityLength(0.393701, LengthUnit.Inch);
 
-        Console.WriteLine(q1.Equals(q2)); // true
+        Console.WriteLine(yard.Equals(feet));   // true
+        Console.WriteLine(yard.Equals(inches)); // true
+        Console.WriteLine(cm.Equals(inch));     // true
     }
     }
